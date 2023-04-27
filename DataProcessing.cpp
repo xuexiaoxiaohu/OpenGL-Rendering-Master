77d7ae5ebba7d45aa071822f8c6d1b5c40e36378
zhangbaojun <89057244+BaojunZhangLoveLife@users.noreply.github.com>
2023-04-27 15:54:25 +0800

    1

--- DataProcessing.cpp
+++ DataProcessing.cpp
@@ -164,3 +164,78 @@ void DataProcessing::poly2tri(std::string src, std::string dst) {
 	writer->Update();
 }
 	
+void DataProcessing::isoExpRemeshing(const char* srcPath, const char* dstPath) {
+	vtkSmartPointer<vtkPLYReader> reader = vtkSmartPointer<vtkPLYReader>::New();
+	reader->SetFileName(srcPath);
+	reader->Update();
+	auto pd = reader->GetOutput();
+	vtkSmartPointer<vtkImageData> whiteImage = vtkSmartPointer<vtkImageData>::New();
+	double bounds[6];
+	pd->GetBounds(bounds);
+	double spacing[3]; // desired volume spacing
+	spacing[0] = 0.5;
+	spacing[1] = 0.5;
+	spacing[2] = 0.5;
+	whiteImage->SetSpacing(spacing);
+
+	// compute dimensions
+	int dim[3];
+	for (int i = 0; i < 3; i++)
+	{
+		dim[i] = static_cast<int>(
+			ceil((bounds[i * 2 + 1] - bounds[i * 2]) / spacing[i]));
+	}
+	whiteImage->SetDimensions(dim);
+	whiteImage->SetExtent(0, dim[0] - 1, 0, dim[1] - 1, 0, dim[2] - 1);
+
+	double origin[3];
+	origin[0] = bounds[0] + spacing[0] / 2;
+	origin[1] = bounds[2] + spacing[1] / 2;
+	origin[2] = bounds[4] + spacing[2] / 2;
+	whiteImage->SetOrigin(origin);
+	whiteImage->AllocateScalars(VTK_UNSIGNED_CHAR, 1);
+
+	// fill the image with foreground voxels:
+	unsigned char inval = 255;
+	unsigned char outval = 0;
+	vtkIdType count = whiteImage->GetNumberOfPoints();
+	for (vtkIdType i = 0; i < count; ++i)
+	{
+		whiteImage->GetPointData()->GetScalars()->SetTuple1(i, inval);
+	}
+	// polygonal data --> image stencil:
+	vtkSmartPointer<vtkPolyDataToImageStencil> pol2stenc = vtkSmartPointer<vtkPolyDataToImageStencil>::New();
+	pol2stenc->SetInputData(pd);
+	pol2stenc->SetOutputOrigin(origin);
+	pol2stenc->SetOutputSpacing(spacing);
+	pol2stenc->SetOutputWholeExtent(whiteImage->GetExtent());
+	pol2stenc->Update();
+
+	// cut the corresponding white image and set the background:
+	vtkSmartPointer<vtkImageStencil> imgstenc = vtkSmartPointer<vtkImageStencil>::New();
+
+	imgstenc->SetInputData(whiteImage);
+	imgstenc->SetStencilConnection(pol2stenc->GetOutputPort());
+	imgstenc->ReverseStencilOff();
+	imgstenc->SetBackgroundValue(outval);
+	imgstenc->Update();
+
+	vtkSmartPointer<vtkImageGaussianSmooth> gaussianSmoothFilter = vtkSmartPointer<vtkImageGaussianSmooth>::New();
+	gaussianSmoothFilter->SetInputConnection(imgstenc->GetOutputPort());
+	gaussianSmoothFilter->SetDimensionality(3);
+	gaussianSmoothFilter->SetRadiusFactor(5);
+	gaussianSmoothFilter->SetStandardDeviation(1);
+	gaussianSmoothFilter->Update();//这是添加的图像平滑处理，高斯平滑
+
+	vtkSmartPointer<vtkMarchingCubes>marchingcube = vtkSmartPointer<vtkMarchingCubes>::New();
+	marchingcube->SetInputData(gaussianSmoothFilter->GetOutput());
+	marchingcube->SetValue(0, 70);
+	marchingcube->ComputeNormalsOn();//计算表面法向量;
+	marchingcube->Update();
+
+	vtkSmartPointer<vtkPLYWriter> writer = vtkSmartPointer<vtkPLYWriter>::New();
+	writer->SetFileName(dstPath);
+	writer->SetInputData(marchingcube->GetOutput());
+	writer->SetFileTypeToASCII();
+	writer->Update();
+}
--- DataProcessing.h
+++ DataProcessing.h
@@ -12,12 +12,23 @@
 #include <pcl/features/normal_3d_omp.h>
 #include <pcl/io/vtk_lib_io.h>
 //VTK
-#include <vtkSTLReader.h>
-#include <vtkSTLWriter.h>
-#include <vtkPLYReader.h>
-#include <vtkPLYWriter.h>
 #include <vtkTriangleFilter.h>
 #include <vtkFillHolesFilter.h>
+#include <vtkPLYReader.h>
+#include <vtkSmartPointer.h>
+#include <vtkImageData.h>
+#include <vtkPointData.h>
+#include <vtkUnsignedCharArray.h>
+#include <vtkSmartPointer.h>
+#include <vtkXMLImageDataWriter.h>
+#include <vtkPLYReader.h>
+#include <vtkPLYWriter.h>
+#include <vtkPolyDataToImageStencil.h>
+#include <vtkImageStencil.h>
+#include <vtkMetaImageWriter.h>
+#include <vtkMarchingCubes.h>
+#include <vtkImageGaussianSmooth.h>
+#include <vtkStripper.h>
 
 class DataProcessing {
 public:
@@ -36,7 +47,8 @@ public:
 	void fillMesh(pcl::PolygonMesh& mesh);
 	void getErasedMesh(QVector3D worldPos, pcl::PolygonMesh &mesh, std::vector<QVector3D> vertices, float radius);
 	void getRenderData(pcl::PolygonMesh &mesh);
-
+	//Remeshing: Isotropic Explicit Remeshing
+	void isoExpRemeshing(const char* srcPath,const char* dstPath);
 	std::vector<QVector3D>	pointData;
 	std::vector<float> glMeshData;
 	QVector3D maxPoint;
